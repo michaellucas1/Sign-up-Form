d4dc45f271360119dea8a44e61c85141461e0522
John <141792618+michaellucas2008@users.noreply.github.com>
2024-08-16 23:53:04 -0400

    refactor the logic that displays the error message. The error message is now embeded to the html  and the javascript just need to turn it visible when the password is mismatched and hidden when password is a matched

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
 const password = document.querySelectorAll('input[type="password"');
+const message =document.querySelectorAll(".form-column > span");
 password.forEach((currentValue)=>{
     currentValue.addEventListener("keyup",(event)=>{
         const numberOfNode=currentValue.parentElement.childNodes.length;
+        
         if(password[1].value!==password[0].value){       
             password[0].classList="invalid";
             password[1].classList="invalid";
-            if(numberOfNode<=5){
-                const message =document.createElement("span");
-                message.textContent="* Passwords do not match";
-                message.style.fontSize="12px";
-                message.style.color="red";
-                currentValue.parentElement.appendChild(message);
-            }     
+            message[0].style.visibility="visible";
+            message[1].style.visibility="visible";
         }
         else if(password[1].value===password[0].value &&password[1].value !=="" &&password[0].value !==""){
             password[0].classList="valid";
             password[1].classList="valid";
-            const passOneLength = password[0].parentElement.childNodes.length;
-            const passTwoLength = password[1].parentElement.childNodes.length;
-            if(passOneLength>=6){
-                password[0].parentElement.removeChild(password[0].parentElement.lastElementChild);
-            }
-            if(passTwoLength>=6){
-                password[1].parentElement.removeChild(password[1].parentElement.lastElementChild);
-            }
+            message[0].style.visibility="hidden";
+            message[1].style.visibility="hidden";
         }
     });
 });
